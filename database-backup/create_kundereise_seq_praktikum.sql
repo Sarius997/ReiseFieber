bc6169910ec14816c0f4bc4bb3bb233133e4091f
Sarius997 <sarius997@gmail.com>
2015-05-29 11:38:01 +0200

    added database sql statements from my week at micro epsilon

---
+++ database-backup/create_kundereise_seq_praktikum.sql
@@ -0,0 +1,12 @@
+﻿-- Sequence: kundereiseid
+
+-- DROP SEQUENCE kundereiseid;
+
+CREATE SEQUENCE kundereiseid
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 10
+  CACHE 1;
+ALTER TABLE kundereiseid
+  OWNER TO postgres;
---
+++ database-backup/create_table(kunden)_praktikum.sql
@@ -0,0 +1,23 @@
+﻿-- Table: kunden
+
+-- DROP TABLE kunden;
+
+CREATE TABLE kunden
+(
+  id integer NOT NULL DEFAULT nextval('kdid'::regclass),
+  nachname character varying(128),
+  vorname character varying(128),
+  wohnort character varying(128),
+  geburtstag character varying(128),
+  volljaehrig character varying(128),
+  telefonnummer character varying(128),
+  geschlecht character varying(128),
+  postleitzahl text,
+  adresse text,
+  CONSTRAINT kunden_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE kunden
+  OWNER TO postgres;
