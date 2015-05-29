b5fb9e3a36c360b7ba20fb0c5b661f42ddceb916
Sarius997 <sarius997@gmail.com>
2015-05-29 20:30:04 +0200

    Revert "added menu to main window"
    This reverts commit 9b1f12efb2dd0ceac4ee2dda235154b79c0ef7f5.

--- Reisefieber/src/gui/ReiseFieberGUI.java
+++ Reisefieber/src/gui/ReiseFieberGUI.java
@@ -1,22 +1,13 @@
 package gui;
 
-import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 
-import javax.swing.BorderFactory;
-import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JTextField;
 
 public class ReiseFieberGUI {
-
-	private JMenuBar menuBar;
 
 	private JFrame frame;
 	private JButton eingabe;
@@ -26,9 +17,6 @@ public class ReiseFieberGUI {
 	private JButton beenden;
 
 	public ReiseFieberGUI() {
-		BorderLayout border = new BorderLayout();
-		
-
 		frame = new JFrame("ReiseFieber");
 		eingabe = new JButton("Kunde erstellen");
 		suche = new JButton("Kunden suchen");
@@ -36,26 +24,19 @@ public class ReiseFieberGUI {
 		reise = new JButton("Kunden zu Reise hinzufügen");
 		beenden = new JButton("EXIT");
 
-		menuBar = new JMenuBar();
-		menuBar.add(eingabe);
-		menuBar.add(suche);
-		menuBar.add(aendern);
-		menuBar.add(reise);
-		menuBar.add(beenden);
-
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(border);
-		
-		frame.add(new JTextField());
-
-		frame.setJMenuBar(menuBar);
-
+		frame.setLayout(new GridLayout(1, 5));
+		frame.add(eingabe);
+		frame.add(suche);
+		frame.add(aendern);
+		frame.add(reise);
+		frame.add(beenden);
 		addActionListeners();
 
 	}
 
 	public void show() {
-		// TODO rework
+		//TODO rework
 		frame.pack();
 		frame.show();
 	}
